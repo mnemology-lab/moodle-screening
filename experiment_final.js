c00dc430a6ed123d2ae8844c44be8ef4405c77c3
catherinesaunders <cgs47@duke.edu>
2025-11-20 11:53:53 -0500

    Update experiment_final.js

--- experiment_final.js
+++ experiment_final.js
@@ -69,9 +69,9 @@ const mooney_trial_template = {
         // C. CATEGORY RESPONSE (5 seconds max) - FIX APPLIED
         {
             type: jsPsychHtmlKeyboardResponse,
-            stimulus: function(){
-                // FIX: Get variable first, then call replace on the variable
-                const category_options = jsPsych.timelineVariable('category_choices');
+            stimulus: function(trial){
+                // *** FINAL FIX: Accessing parameters directly from the trial object/settings ***
+                const category_options = trial.category_choices;
                 return '<p style="font-size: 24px;">Choose the correct category (Press 1-5):</p>' + '<div class="stimulus-text-container">' + category_options.replace(/\n/g, '<br>') + '</div>';
             },
             
@@ -88,9 +88,9 @@ const mooney_trial_template = {
         // D. OBJECT RESPONSE (5 seconds max) - FIX APPLIED
         {
             type: jsPsychHtmlKeyboardResponse,
-            stimulus: function(){
-                // FIX: Get variable first, then call replace on the variable
-                const object_options = jsPsych.timelineVariable('object_choices');
+            stimulus: function(trial){
+                // *** FINAL FIX: Accessing parameters directly from the trial object/settings ***
+                const object_options = trial.object_choices;
                 return '<p style="font-size: 24px;">Choose the exact object (Press 1-5):</p>' + '<div class="stimulus-text-container">' + object_options.replace(/\n/g, '<br>') + '</div>';
             },
             choices: ['1', '2', '3', '4', '5'],
