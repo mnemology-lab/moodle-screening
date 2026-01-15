eee6ff841b75930d9b247128f932d108f1a552c9
mnemology-lab <cgs47@duke.edu>
2026-01-15 16:32:36 -0500

    Update battery_task.js

--- battery_task.js
+++ battery_task.js
@@ -28,13 +28,13 @@ const raven_items = [
 // Moodle Items (from experiment_final.js)
 const moodle_items = [
     { stimulus: 'A_cougar_sigma_3.jpg', correct_category_key: '1', correct_object_key: '3', category_choices: '1) mammal\n2) insect\n3) reptile\n4) household item\n5) bird', object_choices: '1) bunny\n2) rat\n3) cougar\n4) mountain\n5) crocodile' },
-    { stimulus: 'A_deer_sigma_3.jpg', correct_category_key: '1', correct_object_key: '2', category_choices: '1) mammal\n2) insect\n3) reptile\n4) household item\n5) bird', object_choices: '1) squirrel\n2) deer\n3) horse\n4) dog\n5) wolf' },
-    { stimulus: 'A_eagle_sigma_3.jpg', correct_category_key: '5', correct_object_key: '4', category_choices: '1) mammal\n2) insect\n3) reptile\n4) household item\n5) bird', object_choices: '1) owl\n2) penguin\n3) parrot\n4) eagle\n5) sparrow' },
-    { stimulus: 'A_fly_sigma_3.jpg', correct_category_key: '2', correct_object_key: '1', category_choices: '1) mammal\n2) insect\n3) reptile\n4) household item\n5) bird', object_choices: '1) fly\n2) bee\n3) butterfly\n4) spider\n5) ant' },
-    { stimulus: 'A_kettle_sigma_3.jpg', correct_category_key: '4', correct_object_key: '3', category_choices: '1) mammal\n2) insect\n3) reptile\n4) household item\n5) bird', object_choices: '1) toaster\n2) pot\n3) kettle\n4) plate\n5) spoon' },
-    { stimulus: 'A_lizard_sigma_3.jpg', correct_category_key: '3', correct_object_key: '5', category_choices: '1) mammal\n2) insect\n3) reptile\n4) household item\n5) bird', object_choices: '1) snake\n2) turtle\n3) frog\n4) chameleon\n5) lizard' },
-    { stimulus: 'A_piano_sigma_3.jpg', correct_category_key: '4', correct_object_key: '2', category_choices: '1) mammal\n2) insect\n3) reptile\n4) household item\n5) bird', object_choices: '1) guitar\n2) piano\n3) drum\n4) violin\n5) flute' },
-    { stimulus: 'A_spider_sigma_3.jpg', correct_category_key: '2', correct_object_key: '4', category_choices: '1) mammal\n2) insect\n3) reptile\n4) household item\n5) bird', object_choices: '1) beetle\n2) scorpion\n3) crab\n4) spider\n5) mosquito' }
+    { stimulus: 'A_bee_sigma_7.jpg', correct_category_key: '1', correct_object_key: '3', category_choices: '1) insect\n2) mammal\n3) reptile\n4) household item\n5) bird', object_choices: '1) spider\n2) cactus\n3) bee\n4) clown\n5) octopus' },
+    { stimulus: '0_dolphin.new_gauss3.jpg', correct_category_key: '2', correct_object_key: '4', category_choices: '1) insect\n2) mammal\n3) reptile\n4) household item\n5) bird', object_choices: '1) duck\n2) ant\n3) crocodile\n4) dolphin\n5) horse' },
+    { stimulus: '0_ant_gauss2.jpg', correct_category_key: '2', correct_object_key: '4', category_choices: '1) bird\n2) insect\n3) reptile\n4) household item\n5) mammal', object_choices: '1) bat\n2) butterfly\n3) dog\n4) ant\n5) chicken' },
+    { stimulus: '0_pigeon_filt1_gauss2.jpg', correct_category_key: '4', correct_object_key: '5', category_choices: '1) mammal\n2) insect\n3) reptile\n4) bird\n5) household item', object_choices: '1) oyster\n2) leaf\n3) nursery\n4) turtle\n5) pigeon' },
+    { stimulus: '0_sloth_gauss4.jpg', correct_category_key: '4', correct_object_key: '5', category_choices: '1) household item\n2) insect\n3) reptile\n4) mammal\n5) bird', object_choices: '1) squirrel\n2) crab\n3) monkey\n4) panda\n5) sloth' },
+    { stimulus: '0_snake2_new_gauss2.jpg', correct_category_key: '5', correct_object_key: '2', category_choices: '1) mammal\n2) insect\n3) bird\n4) household item\n5) reptile', object_choices: '1) sailing boat\n2) snake\n3) tooth brush\n4) duck\n5) beetle' },
+    { stimulus: '0_wateringcan_gauss4.jpg', correct_category_key: '5', correct_object_key: '2', category_choices: '1) mammal\n2) insect\n3) reptile\n4) bird\n5) household item', object_choices: '1) desktop\n2) watering can\n3) cabin\n4) knife\n5) lantern' }
 ];
 
 // Preload Images
