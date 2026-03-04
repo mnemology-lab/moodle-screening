fddd188e65a4fbf72a05c178a3cee567fbb8b8a6
mnemology-lab <cgs47@duke.edu>
2026-03-04 15:18:38 -0500

    Update battery_task-2.js

--- battery_task-2.js
+++ battery_task-2.js
@@ -54,7 +54,7 @@ const raven_practice_images = [
 // --- Moodle Items ---
 const moodle_items = [
     { stimulus: 'tomatoesmoonified.jpg', correct_category_key: '1', correct_object_key: '3', category_choices: '1) plant or food\n2) insect\n3) reptile\n4) household item\n5) mammal', object_choices: '1) bunny\n2) wheat\n3) tomatoes\n4) pretzel\n5) crocodile' },
-    { stimulus: 'pumpkin2moonified.jpg', correct_category_key: '1', correct_object_key: '3', category_choices: '1) plant or food\n2) mammal\n3) reptile\n4) household item\n5) bird', object_choices: '1) spider\n2) cactus\n3) pumpkin\n4) clown\n5) octopus' },
+    { stimulus: '0_pumpkin2moonified.jpg', correct_category_key: '1', correct_object_key: '3', category_choices: '1) plant or food\n2) mammal\n3) reptile\n4) household item\n5) bird', object_choices: '1) spider\n2) cactus\n3) pumpkin\n4) clown\n5) octopus' },
     { stimulus: '0_dolphin.new_gauss3.jpg', correct_category_key: '2', correct_object_key: '4', category_choices: '1) insect\n2) mammal\n3) reptile\n4) household item\n5) bird', object_choices: '1) duck\n2) ant\n3) crocodile\n4) dolphin\n5) horse' },
     { stimulus: '0_ant_gauss2.jpg', correct_category_key: '2', correct_object_key: '4', category_choices: '1) bird\n2) insect\n3) reptile\n4) household item\n5) plant or food', object_choices: '1) bat\n2) butterfly\n3) dog\n4) ant\n5) chicken' },
     { stimulus: '0_pigeon_filt1_gauss2.jpg', correct_category_key: '4', correct_object_key: '5', category_choices: '1) mammal\n2) insect\n3) reptile\n4) bird\n5) household item', object_choices: '1) oyster\n2) leaf\n3) nursery\n4) turtle\n5) pigeon' },
